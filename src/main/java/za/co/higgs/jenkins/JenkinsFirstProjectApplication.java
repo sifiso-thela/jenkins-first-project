691b5bce81ff910966efdb49391c2482234f56ae
sifiso-thela <sifiso.thela@falcorp.co.za>
2021-02-08 09:29:52 +0200

    first commit

---
+++ src/main/java/za/co/higgs/jenkins/JenkinsFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package za.co.higgs.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsFirstProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/higgs/jenkins/JenkinsFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.higgs.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
